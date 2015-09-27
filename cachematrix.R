ba638d758e83fe26ea8299e6d71ea0a449929c72
cfvandev <cfvandev@yahoo.com>
2015-09-27 21:37:30 +0100

    27 Sep 2015

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-# cfvandev 22/02/2015
+# cfvandev 27/09/2015
 # Caches the inverse of a matrix so that if the matrix does not change
 # the inverse can be looked up from the cache instead of re-calculated
 
